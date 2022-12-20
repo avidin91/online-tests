11f1c6ba743e0dc538134c650502bf166c5fc441
avidin91 <a.vidin.91@gmail.com>
2022-12-20 14:46:06 +0300

    Project Launch

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <div className={classes.head}>
+            <div className={classes.logo}>
+                <img src="" alt=""/>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/OnlineTests.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OnlineTests = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OnlineTests;
